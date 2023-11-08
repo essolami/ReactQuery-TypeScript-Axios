6997cc76cf9a9a9606c44cbcd2ee8727b2fe780d
essolami <essolamih@gmail.com>
2023-11-08 09:54:09 +0100

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: 1,
+      staleTime: 5 * 1000,
+    },
+  },
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+    <ReactQueryDevtools initialIsOpen={false} />
   </React.StrictMode>
 );
 
---
+++ src/pages/home.page.tsx
@@ -0,0 +1,62 @@
+import { Box, Container, Grid } from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import { getAllPostsFn } from "../api/postApi";
+import FullScreenLoader from "../components/FullScreenLoader";
+import PostItem from "../components/post/post.component";
+import Message from "../components/Message";
+
+const HomePage = () => {
+  const { isLoading, data: posts } = useQuery(
+    ["posts"],
+    () => getAllPostsFn(),
+    {
+      select: (data) => data.data.posts,
+      onError: (error) => {
+        if (Array.isArray((error as any).data.error)) {
+          (error as any).data.error.forEach((el: any) =>
+            toast.error(el.message, {
+              position: "top-right",
+            })
+          );
+        } else {
+          toast.error((error as any).data.message, {
+            position: "top-right",
+          });
+        }
+      },
+    }
+  );
+
+  if (isLoading) {
+    return <FullScreenLoader />;
+  }
+
+  return (
+    <Container
+      maxWidth={false}
+      sx={{ backgroundColor: "#2363eb", minHeight: "100vh" }}
+    >
+      {posts?.length === 0 ? (
+        <Box maxWidth="sm" sx={{ mx: "auto", py: "5rem" }}>
+          <Message type="info" title="Info">
+            No posts at the moment
+          </Message>
+        </Box>
+      ) : (
+        <Grid
+          container
+          rowGap={5}
+          maxWidth="lg"
+          sx={{ margin: "0 auto", pt: "5rem" }}
+        >
+          {posts?.map((post) => (
+            <PostItem key={post.id} post={post} />
+          ))}
+        </Grid>
+      )}
+    </Container>
+  );
+};
+
+export default HomePage;
